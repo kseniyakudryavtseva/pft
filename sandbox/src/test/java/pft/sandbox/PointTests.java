0f7ba51880e4e1a92e0e6401ae92f77649105830
Kseniya Kudryavtseva <kseniya_2905@mail.ru>
2019-05-21 22:51:52 +0400

    Задание 3_создание первых тестов
    Проверка корректности расчета расстояния между двумя точками

---
+++ sandbox/src/test/java/pft/sandbox/PointTests.java
@@ -0,0 +1,26 @@
+package pft.sandbox;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class PointTests {
+
+    @Test
+    public void testDistance1(){
+        Point p1 = new Point(2,4);
+        Point p2 = new Point(5,6);
+        Assert.assertEquals(p1.distance(p2), Math.sqrt(13));
+    }
+    @Test
+    public void testDistance2(){
+        Point p1 = new Point(0,4);
+        Point p2 = new Point(0,4);
+        Assert.assertEquals(p1.distance(p2), 0.0);
+    }
+    @Test
+    public void testDistance3(){
+        Point p1 = new Point(0,0);
+        Point p2 = new Point(0,1);
+        assert p1.distance(p2) == 1.0;
+    }
+}
